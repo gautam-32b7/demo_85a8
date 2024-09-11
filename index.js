8d57556f36fd042769bc8ba0e90df644e8fb354b
Gautam Sarkar <gautam_32b7@hotmail.com>
2024-09-12 04:57:19 +0530

    fix ejs view engine

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index');
 });
 
 app.listen(port, () => {
