2d4cbe27d2c710536838bfa9808101ab9f0d7f34
Gautam Sarkar <gautam_32b7@hotmail.com>
2024-09-12 05:33:07 +0530

    update index.ejs file

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+import { __express as ejsEngine } from 'ejs';
+
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.engine('ejs', ejsEngine);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render(__dirname + '/views/index.ejs');
